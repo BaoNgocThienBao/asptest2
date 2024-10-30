7ea22a82a245ddfca7217087692e0ed60e840ed2
BaoNgocThienBao <1721031139@dntu.edu.vn>
2024-10-30 16:45:41 +0700

    add

---
+++ FINAL_TEST/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using FINAL_TEST.Controllers;
+using FINAL_TEST.Data;
+using FINAL_TEST.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+
+namespace FINAL_TEST.Controllers
+{
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<SanPham> sanpham = _db.SanPham.ToList();
+            return View(sanpham);
+        }
+        [HttpGet]
+        public IActionResult Details(int sanphamId)
+        {
+            GioHang giohang = new GioHang()
+            {
+                SanPhamId = sanphamId,
+                SanPham = _db.SanPham.Include("TheLoai").FirstOrDefault(sp => sp.Id == sanphamId),
+                Quantity = 1
+            };
+
+            return RedirectToAction("Details", "Shop",giohang) ;
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
